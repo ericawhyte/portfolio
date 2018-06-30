b8ac40fa275958de354e5b4f6fec017367889503
EricaWhyte <erica_whyte@hotmail.com>
2018-06-29 21:15:19 -0400

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+
+const NotFoundPage = () => (
+  <Container>
+    <h1>NOT FOUND</h1>
+    <p>The cheekiness...</p>
+  </Container>
+)
+
+export default NotFoundPage
